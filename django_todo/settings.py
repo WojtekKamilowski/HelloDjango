8f65ba877f5f3d776c0b9bc7a56ad0306ceea8c3
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-03-02 07:50:09 +0000

    Add todo list template

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 if os.path.exists("env.py"):
     import env
 
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
